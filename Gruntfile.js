6a1245ec9d4765f84334f63837cd3f2ddf1f0f71
Frederick Fogerty <freelyfred@gmail.com>
2014-01-25 15:41:15 +1300

    More Gruntfile.js changes, and using non minified phaser js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
             main: {
                 files: [
                     {cwd: 'src/', expand: true, src: ['*.html'], dest: 'dev/'},
-                    {cwd: 'src/res/public/noupscale', expand: true, src: ['*.json', '*.ttf', '*.otf'], dest: 'dev/res/'}
+                    {cwd: 'src/res/public/noupscale', expand: true, src: ['**'], dest: 'dev/res/'}
                 ],
                 nonull: true
             },
